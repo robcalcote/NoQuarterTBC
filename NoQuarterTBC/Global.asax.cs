6c1cf0d2e4b58652c15d14f67c415e63b3430e2a
Rob Calcote <robcalcote@gmail.com>
2018-12-14 21:54:02 -0700

    Added DAL (fully built) and edited the web.config and global.asax files (also built)

--- NoQuarterTBC/Global.asax.cs
+++ NoQuarterTBC/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NoQuarterTBC.Models;
+using NoQuarterTBC.DAL;
+using System.Data.Entity;
 
 namespace NoQuarterTBC
 {
@@ -12,6 +15,8 @@ namespace NoQuarterTBC
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<NoQuarterTBCContext>(null);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
