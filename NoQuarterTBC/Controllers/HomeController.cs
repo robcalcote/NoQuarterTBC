79a2dcd16ce1e46b4136190da9e302cc95f86b5d
Rob Calcote <robcalcote@gmail.com>
2018-12-14 21:54:41 -0700

    Added a site description and also installed Entity Framework

--- NoQuarterTBC/Controllers/HomeController.cs
+++ NoQuarterTBC/Controllers/HomeController.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+/**************************************************************************************************
+ *  Description:    Guild Website for No Quarter (NA)
+ *  Build:          0.1
+ *  DB Hosted On:   Microsoft Azure
+ *  DB Diagram:     https://www.lucidchart.com/documents/edit/2fe6e207-765c-4a10-8167-666a6e089b3c/1
+ *  Source Code:    https://github.com/robcalcote/NoQuarterTBC
+ *  Collaborators:  Alexander Miner, Rob Calcote
+ *************************************************************************************************/
+
 namespace NoQuarterTBC.Controllers
 {
     public class HomeController : Controller
