de17a00df2d9b84ef29798e43dc45e3598020f6b
Rob <robcalcote@gmail.com>
2018-12-15 13:05:37 -0700

    Added a AdminHome page and a link from the main page to navigate there. additionally tied in all admin controllers/views for one central admin page

---
+++ NoQuarterTBC/Controllers/Admin/AdminHomeController.cs
@@ -0,0 +1,27 @@
+﻿using NoQuarterTBC.DAL;
+using NoQuarterTBC.Models;
+using System.Data;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// This is the main Admin Page
+// It will contain links to any of the CRUD 
+// views for each table within the database.
+
+namespace NoQuarterTBC.Controllers.Admin
+{
+    public class AdminHomeController : Controller
+    {
+        private NoQuarterTBCContext db = new NoQuarterTBCContext();
+
+        // GET: AdminHome
+        public ActionResult Index()
+        {
+            return View("~/Views/Admin/AdminHome/Index.cshtml");
+        }
+    }
+}
